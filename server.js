7d74bba5e646696c98be3b662f485ccb5c0b60c5
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-01-12 21:13:31 +0530

    preparing for deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const connectDB = require('./config/db');
 
+const path = require('path');
+
 const app = express();
 
 //Connect Database
@@ -15,7 +17,16 @@ app.use('/list', require('./routes/api/list'));
 app.use('/count', require('./routes/api/count'));
 app.use('/search', require('./routes/api/search'));
 
-app.get('/', (req, res) => res.send('API running'));
+// Serve static assets i.e. React(Front End) build folder in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+    // changed the above line from path.res(__dirname, 'client', 'build', 'index.html')); to allow reloading of the site & load through URL
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
